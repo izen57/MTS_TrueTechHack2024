fac2e2bdfc4638ad6c8657c031ee840e156683a5
izen57 <golovoslon@gmail.com>
2024-04-28 23:53:06 +0200

    fix: disable internal authentication and `x-frame` related parameter
    Co-authored-by: Tulenien timofey.evsigneev@gmail.com

--- backend/src/main/java/org/glabs/accessibility/AccessibilityApplication.java
+++ backend/src/main/java/org/glabs/accessibility/AccessibilityApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccessibilityApplication {
-
     public static void main(String[] args) {
+        // перейдите на http://localhost:8080/h2-console, чтоб управлять БД "H2"
         SpringApplication.run(AccessibilityApplication.class, args);
     }
-
 }
---
+++ backend/src/main/java/org/glabs/accessibility/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.glabs.accessibility;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user =
+            User.withUsername("user")
+                .password(encoder().encode("pass"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+    }
+}
+
